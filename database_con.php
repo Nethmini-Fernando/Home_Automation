a5ca1a7303dc5c8c85e3e695a702d97d45d33289
Nethmini-Fernando <100115628+Nethmini-Fernando@users.noreply.github.com>
2022-03-19 07:08:51 +0530

    Update database_con.php

--- database_con.php
+++ database_con.php
@@ -1,9 +1,9 @@
 <?php 
 //connecting the database
-$server = "localhost";
-$user = "root";
-$pass = "";
-$database = "home_automation_db";
+$server = "localhost";  //server name in my phpmyadmin
+$user = "root"; //user name in my phpmyadmin.
+$pass = ""; //no-password in my phpmyadmin.
+$database = "home_automation_db"; //name of the database
 $db_con = mysqli_connect($server, $user, $pass, $database);
 
 if (!$db_con) {
