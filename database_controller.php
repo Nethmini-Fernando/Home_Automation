a0b2726a1274dce8fa60143b7d42a6c0f3b0e0bf
Nethmini-Fernando <100115628+Nethmini-Fernando@users.noreply.github.com>
2022-03-19 07:11:33 +0530

    Update database_controller.php

--- database_controller.php
+++ database_controller.php
@@ -50,3 +50,140 @@ if(isset($_POST['signup'])){
     }
 
 }
+    //if user click verification code submit button
+    if(isset($_POST['check'])){
+        $_SESSION['info'] = "";
+        $otp_code = mysqli_real_escape_string($db_con, $_POST['otp']);
+        $check_code = "SELECT * FROM users_reg WHERE code = $otp_code";
+        $code_res = mysqli_query($db_con, $check_code);
+        if(mysqli_num_rows($code_res) > 0){
+            $fetch_data = mysqli_fetch_assoc($code_res);
+            $fetch_code = $fetch_data['code'];
+            $email = $fetch_data['email'];
+            $code = 0;
+            $status = 'verified';
+            $update_otp = "UPDATE users_reg SET code = $code, status = '$status' WHERE code = $fetch_code";
+            $update_res = mysqli_query($db_con, $update_otp);
+            if($update_res){
+                $_SESSION['name'] = $name;
+                $_SESSION['email'] = $email;
+                header('location: home.php');
+                exit();
+            }else{
+                $errors['otp-error'] = "Failed while updating code!";
+            }
+        }else{
+            $errors['otp-error'] = "You've entered incorrect code!";
+        }
+    }
+
+    //if user click login button
+    if(isset($_POST['login'])){
+        $email = mysqli_real_escape_string($db_con, $_POST['email']);
+        $password = mysqli_real_escape_string($db_con, $_POST['password']);
+        $check_email = "SELECT * FROM users_reg WHERE email = '$email'";
+        $email_recheck = mysqli_query($db_con, $check_email);
+        if(mysqli_num_rows($email_recheck) > 0){
+            $fetch = mysqli_fetch_assoc($email_recheck);
+            $fetch_pass = $fetch['password'];
+            if(password_verify($password, $fetch_pass)){
+                $_SESSION['email'] = $email;
+                $status = $fetch['status'];
+                if($status == 'verified'){
+                  $_SESSION['email'] = $email;
+                  $_SESSION['password'] = $password;
+                    header('location: home.php');
+                }else{
+                    $info = "It's look like you haven't still verify your email - $email";
+                    $_SESSION['info'] = $info;
+                    header('location: user-otp.php');
+                }
+            }else{
+                $errors['email'] = "Incorrect email or password!";
+            }
+        }else{
+            $errors['email'] = "It's look like you're not yet a member! Click on the bottom link to signup.";
+        }
+        
+        $insert_data = "INSERT INTO users_log (email)
+                        values('$email')";
+        $data_check = mysqli_query($db_con, $insert_data);
+    }
+
+    //if user click continue button in forgot password form
+    if(isset($_POST['check-email'])){
+        $email = mysqli_real_escape_string($db_con, $_POST['email']);
+        $check_email = "SELECT * FROM users_reg WHERE email='$email'";
+        $run_sql = mysqli_query($db_con, $check_email);
+        if(mysqli_num_rows($run_sql) > 0){
+            $code = rand(999999, 111111);
+            $insert_code = "UPDATE users_reg SET code = $code WHERE email = '$email'";
+            $run_query =  mysqli_query($db_con, $insert_code);
+            if($run_query){
+                $subject = "Password Reset Code";
+                $message = "Your password reset code is $code";
+                $sender = "From: shahiprem7890@gmail.com";
+                if(mail($email, $subject, $message, $sender)){
+                    $info = "We've sent a passwrod reset otp to your email - $email";
+                    $_SESSION['info'] = $info;
+                    $_SESSION['email'] = $email;
+                    header('location: reset-code.php');
+                    exit();
+                }else{
+                    $errors['otp-error'] = "Failed while sending code!";
+                }
+            }else{
+                $errors['db-error'] = "Something went wrong!";
+            }
+        }else{
+            $errors['email'] = "This email address does not exist!";
+        }
+    }
+
+    //if user click check reset otp button
+    if(isset($_POST['check-reset-otp'])){
+        $_SESSION['info'] = "";
+        $otp_code = mysqli_real_escape_string($db_con, $_POST['otp']);
+        $check_code = "SELECT * FROM users_reg WHERE code = $otp_code";
+        $code_res = mysqli_query($db_con, $check_code);
+        if(mysqli_num_rows($code_res) > 0){
+            $fetch_data = mysqli_fetch_assoc($code_res);
+            $email = $fetch_data['email'];
+            $_SESSION['email'] = $email;
+            $info = "Please create a new password that you don't use on any other site.";
+            $_SESSION['info'] = $info;
+            header('location: new-password.php');
+            exit();
+        }else{
+            $errors['otp-error'] = "You've entered incorrect code!";
+        }
+    }
+
+    //if user click change password button
+    if(isset($_POST['change-password'])){
+        $_SESSION['info'] = "";
+        $password = mysqli_real_escape_string($db_con, $_POST['password']);
+        $confirm_pass= mysqli_real_escape_string($db_con, $_POST['cpassword']);
+        if($password !== $confirm_pass){
+            $errors['password'] = "Confirm password not matched!";
+        }else{
+            $code = 0;
+            $email = $_SESSION['email']; //getting this email using session
+            $encpass = password_hash($password, PASSWORD_BCRYPT);
+            $update_pass = "UPDATE users_reg SET code = $code, password = '$encpass' WHERE email = '$email'";
+            $run_query = mysqli_query($db_con, $update_pass);
+            if($run_query){
+                $info = "Your password changed. Now you can login with your new password.";
+                $_SESSION['info'] = $info;
+                header('Location: password-changed.php');
+            }else{
+                $errors['db-error'] = "Failed to change your password!";
+            }
+        }
+    }
+    
+   //if login now button click
+    if(isset($_POST['login-now'])){
+        header('Location: login-user.php');
+    }
+?>
